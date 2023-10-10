632dd81ab94078780f78fb4ee32041c0cefc7880
timothyhilton <recore444@gmail.com>
2023-10-10 20:27:45 +1000

    integrate postmark

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -1,10 +1,15 @@
 import { PrismaClient } from '@prisma/client'
 import { hash } from 'bcrypt'
 import { NextResponse } from 'next/server'
+import { Client } from 'postmark';
 
+const postmarkClient = new Client(process.env.POSTMARK_SERVER_TOKEN!)
 const prisma = new PrismaClient()
 
 export async function POST(request: Request) {
+
+    // create account for user
+
     const data = await request.json()
 
     const password = await hash(data.password as string, 12)
@@ -16,6 +21,24 @@ export async function POST(request: Request) {
             password: password
         }
     })
+
+    // send verification email to user
+
+    const emailData = {
+        From: "verify@tabbertrack.com",
+        To: user.email,
+        Subject: "Account verification for TabberTrack",
+        HtmlBody: "hello!",
+    };
+
+    try {
+        const result = await postmarkClient.sendEmail(emailData);
+        console.log('Verification email sent successfully');
+    } 
+    catch (error) {
+        console.error('Error sending email:', error);
+        throw error;
+    }
 
     return new NextResponse("created", data)
 }
