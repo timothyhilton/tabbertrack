096e703fc1b5059ed64fd2870525d613f537d660
timothyhilton <recore444@gmail.com>
2023-10-12 21:49:01 +1000

    avatar in navbar

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -3,6 +3,7 @@ import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import { authOptions } from "./api/auth/[...nextauth]/route";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 export async function NavBar(){
     const session = await getServerSession(authOptions)
@@ -10,12 +11,21 @@ export async function NavBar(){
     return(
         <div>
             {session &&
-                <Popover>
-                    <PopoverTrigger>
-                        <Button variant="outline">{session!.user!.name}</Button>
-                    </PopoverTrigger>
-                    <PopoverContent>Place content for the popover here.</PopoverContent>
-                </Popover>
+                <div className="pt-2 pb-2 border-b-[1px]">
+                    <Popover>
+                        <PopoverTrigger>
+                            <Button variant="ghost" className="rounded-full w-[2.8rem] h-[2.8rem]">
+                                <Avatar>
+                                    <AvatarImage src="https://github.com/shadcnjkj.png" />
+                                    <AvatarFallback className="bg-slate-800">
+                                        {session!.user!.name!.slice(0, 2).toUpperCase()}
+                                    </AvatarFallback>
+                                </Avatar>
+                            </Button>
+                        </PopoverTrigger>
+                        <PopoverContent>Place content for the popover here.</PopoverContent>
+                    </Popover>
+                </div>
             }
         </div>
     );
