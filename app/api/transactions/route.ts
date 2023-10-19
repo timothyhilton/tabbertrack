cc34bd0250e4b1818c7692a7c78c6c7f75eafa61
timothyhilton <recore444@gmail.com>
2023-10-19 19:34:11 +1000

    made backend logic

--- app/api/transactions/route.ts
+++ app/api/transactions/route.ts
@@ -18,5 +18,32 @@ export async function POST(request: NextRequest) {
 
     if(!hasTwoOrLessDecimalPlaces(data.amount)){ return NextResponse.json({ error: "Amount can't have more than 2 decimal places" }, { status: 400 }) }
     
+    const toUser = await prisma.user.findFirst({
+        where: {
+            username: data.username
+        }
+    })
+    if(!toUser){ return NextResponse.json({ error: `No user exists with username "${data.username}"` }, { status: 400 }) }
+    if(toUser.id == parseInt(session.user!.id)){ return NextResponse.json({ error: "You can't owe money to yourself, silly!" }, { status: 400 }) }
+
+    const friendCheck = await prisma.user.findMany({
+        where: {
+            friend: {
+                some: {
+                    id: parseInt(session.user!.id)
+                }
+            }
+        }
+    })
+    if(!friendCheck.find(user => user.username == data.username)) { return NextResponse.json({ error: `You aren't friends with that user` }, { status: 400 }) }
+
+    await prisma.transaction.create({
+        data: {
+            fromUserId: parseInt(session.user!.id),
+            toUserId: toUser.id,
+            amount: data.amount
+        }
+    })
+
     return NextResponse.json({ success: "Transaction submitted successfully "}, { status: 201 })
 }
