b8efbf6b901eb3c93785ef8be4fbb185695cfa38
timothyhilton <recore444@gmail.com>
2023-10-26 21:35:05 +1000

    updated user page

--- app/users/[username]/page.tsx
+++ app/users/[username]/page.tsx
@@ -21,15 +21,19 @@ export default async function UserPage({ params }: paramProps){
 
     const user = await prisma.user.findFirst({
         where: {
-            username: username
+            username: username,
+            friend: {
+                some: {
+                    id: parseInt(session.user!.id)
+                }
+            }
         }
     })
-
     if(!user){
         redirect('/404')
     }
 
-    const transactions = await Promise.all((await prisma.transaction.findMany({
+    const transactionsForTable = await Promise.all((await prisma.transaction.findMany({
         where: {
             OR: [
                 {
@@ -65,32 +69,80 @@ export default async function UserPage({ params }: paramProps){
         }
     }))
 
-    console.log(transactions)
+
+
+
+    const transactions = await prisma.transaction.findMany({
+        where: {
+            OR: [
+                {
+                    userWhoIsOwedId: parseInt(session!.user!.id),
+                    userWhoOwesId: user.id
+                },
+                {
+                    userWhoOwesId: parseInt(session!.user!.id),
+                    userWhoIsOwedId: user.id
+                }
+            ],
+            status: "accepted"
+        }
+    })
+    
+    let amountOwed = 0
+
+    transactions.forEach(transaction => {
+        if (transaction.userWhoIsOwedId == parseInt(session!.user!.id)) {
+            amountOwed += transaction.amount
+        } else {
+            amountOwed -= transaction.amount
+        }
+    })
 
     return(
         <>
             <NavBar />
             <div className="lg:px-[15vw] flex flex-col mt-[5rem]">
-                <Card className="w-fit pt-6">
-                    <CardContent className="flex flex-row align-middle justify-center">
-                        <Avatar>
-                            <AvatarImage src="https://github.com/shadsdfdcn.png" />
-                            <AvatarFallback>
-                                {username.slice(0, 2).toUpperCase()}
-                            </AvatarFallback>
-                        </Avatar>
-                        <div className="ml-2 mr-3">
-                            <CardTitle>{user.name}</CardTitle>
-                            <CardDescription>{user.username}</CardDescription>
-                        </div>
+                <div className="flex flex-row">
+                    <Card className="w-fit pt-6">
+                        <CardContent className="flex flex-row align-middle justify-center">
+                            <Avatar>
+                                <AvatarImage src="https://github.com/shadsdfdcn.png" />
+                                <AvatarFallback>
+                                    {username.slice(0, 2).toUpperCase()}
+                                </AvatarFallback>
+                            </Avatar>
+                            <div className="ml-2 mr-3">
+                                <CardTitle>{user.name}</CardTitle>
+                                <CardDescription>{user.username}</CardDescription>
+                            </div>
 
-                        <UnfriendButton username={user.username}/>
-                    </CardContent>
-                </Card>
+                            <UnfriendButton username={user.username}/>
+                        </CardContent>
+                    </Card>
+                    <Card className="ml-4 w-fit pt-6">
+                        <CardContent className="">
+                            <CardTitle className="w-full">
+                                { (Math.sign(amountOwed) === 1) ?
+                                    <span className="">{`${user.name} owes you $`}</span> 
+                                    : 
+                                    <span className="">{`You owe ${user.name} $`}</span>
+                                }
+                                {Math.abs(amountOwed)}
+                            </CardTitle>
+                            <CardDescription>
+                                { (Math.sign(amountOwed) === 1) ?
+                                    "Go nag 'em to pay you!"
+                                    : 
+                                    "Go pay 'em then send a transaction here to let them know"
+                                }
+                            </CardDescription>
+                        </CardContent>
+                    </Card>
+                </div>
                 <h1 className="mb-1 mt-8 text-xl flex font-semibold">
                     Transactions
                 </h1>
-                <ReceivedTransactionsTable receivedTransactionRequests={transactions}/>
+                <ReceivedTransactionsTable receivedTransactionRequests={transactionsForTable}/>
             </div>
         </>
     )
