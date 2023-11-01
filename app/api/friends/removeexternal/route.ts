17c911f5928b944fd3fc810b01671544d22405f4
timothyhilton <recore444@gmail.com>
2023-11-01 20:24:00 +1000

    bug fix on deleting external user

--- app/api/friends/removeexternal/route.ts
+++ app/api/friends/removeexternal/route.ts
@@ -20,11 +20,18 @@ export async function POST(request: NextRequest) {
     })
     if(!existingExternalFriend){ return NextResponse.json({ error: "Name not found as external friend" }, { status: 400 }) }
 
+    // first delete all transactions to avoid foreign key restraint fail
+    await prisma.externalTransaction.deleteMany({
+        where: {
+            externalFriendId: existingExternalFriend.id
+        }
+    })
+
     await prisma.externalFriend.delete({
         where: {
             id: existingExternalFriend.id
         }
     })
 
-    return NextResponse.json({ success: "Removed external user successfully"}, { status: 201 })
+    return NextResponse.json({ success: "Removed external user successfully" }, { status: 201 })
 }
