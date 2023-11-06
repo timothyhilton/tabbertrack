9913ab505a80ffeface7b924a2a008d333bc2817
timothyhilton <recore444@gmail.com>
2023-11-06 18:20:23 +1000

    switched around navbar

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -16,13 +16,17 @@ export async function NavBar(){
         <div>
             <div className="lg:px-[15vw] flex-no-wrap relative flex w-full items-center justify-between py-2 border-b-[1px]">
                 <div className="pl-3 flex flex-row">
-                    <Link href="/" className="flex flex-row mt-[0.3rem]">
-                        <Icons.tabbertrack className="w-[1.5rem] h-[1.5rem] my-[0.2rem]" />
-                        <div className="mt-[0.2rem] ml-2 font-semibold">
-                            TabberTrack
-                        </div>
-                    </Link>
-                    <div className="ml-5 mr-1 w-[1px] border-l-[1px]"></div>
+                    { !session &&
+                        <>
+                            <Link href="/" className="flex flex-row mt-[0.3rem]">
+                                <Icons.tabbertrack className="w-[1.5rem] h-[1.5rem] my-[0.2rem]" />
+                                <div className="mt-[0.2rem] ml-2 font-semibold">
+                                    TabberTrack
+                                </div>
+                            </Link>
+                            <div className="ml-5 mr-1 w-[1px] border-l-[1px]"></div>
+                        </>
+                    }
                     <Link href="/dashboard" passHref>
                         <Button variant="link">
                             Home
